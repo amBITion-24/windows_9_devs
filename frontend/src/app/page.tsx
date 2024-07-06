d25abb44e4a31e4e1f8782d98074fb2d5286740d
jo13han <koshy.johan@gmail.com>
2024-07-07 00:15:12 +0530

    frontend: added game to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,44 @@
+'use client'
+import React from "react";
+import { useState } from "react";
 import Particles from "./Particles";
 import Link from "next/link";
 import styles from "./globals.module.css";
 
 export default function Home() {
+
+  const [answer, setAnswer] = useState('');
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const imgs = [
+    { src: '/images/image1.jpg', alt: 'Game Image1', answer: 'Real' },
+    { src: '/images/image2.jpg', alt: 'Game Image2', answer: 'Fake' },
+    { src: '/images/image3.jpg', alt: 'Game Image3', answer: 'Fake' },
+    { src: '/images/image4.jpg', alt: 'Game Image4', answer: 'Real' },
+    { src: '/images/image6.png', alt: 'Game Image6', answer: 'Real' },
+    { src: '/images/image7.png', alt: 'Game Image7', answer: 'Fake' },
+    { src: '/images/image12.jpg', alt: 'Game Image12', answer: 'Fake' },
+    { src: '/images/image14.png', alt: 'Game Image14', answer: 'Fake' },
+    { src: '/images/image19.png', alt: 'Game Image19', answer: 'Real' },
+
+
+  ]
+
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imgs.length);
+    setAnswer('');
+  };
+
+  const handleSelect = (selectedAnswer:string) => {
+    setAnswer(selectedAnswer);
+    if (selectedAnswer === imgs[currentImageIndex].answer) {
+      alert('Correct!');
+    } else {
+      alert('Incorrect!');
+    }
+  }
+
+
   return (
     <>
       <section>
@@ -56,7 +92,39 @@ export default function Home() {
           <Particles />
         </div>
       </section>
-      <section></section>
+      <section className="">
+        <div className="flex flex-col justify-center items-center h-96 bg-black">
+          <h5 className="p-4 font-bold text-5xl text-white">Real or Fake?</h5>
+          <div className="p-4 w-1/2 h-full bg-slate-800 border-2 border-slate-600 rounded-lg flex justify-center items-center">
+            <img src={imgs[currentImageIndex].src} className="w-32 h-32" alt={imgs[currentImageIndex].alt} />
+          </div>
+          <div className="mt-4 flex space-x-4">
+            <button
+              className="px-4 py-2 bg-green-500 text-white font-bold rounded-lg hover:bg-green-700"
+              onClick={() => handleSelect('Real')}
+            >
+              Real
+            </button>
+            <button
+              className="px-4 py-2 bg-red-500 text-white font-bold rounded-lg hover:bg-red-700"
+              onClick={() => handleSelect('Fake')}
+            >
+              Fake
+            </button>
+          </div>
+          <div className="mt-4">
+            <p id="answerPrompt" className="text-xl text-white">{answer && `You selected: ${answer}`}</p>
+          </div>
+          <div className="mt-4">
+            <button
+              className="px-4 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700"
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
